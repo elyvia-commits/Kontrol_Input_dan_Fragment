1364c2c08776ca9e0e12beae72b5cd2b524f86e9
elyvia <elyvia@students.amikom.ac.id>
2025-10-30 18:16:29 +0700

    feat: implement input controls and fragment.

--- app/src/main/java/com/example/myapplication17/RegisterActivity.kt
+++ app/src/main/java/com/example/myapplication17/RegisterActivity.kt
@@ -1,9 +1,6 @@
 package com.example.myapplication17
-
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.widget.Button
+import android.widget.*
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,10 +11,33 @@ class RegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_register)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val spinnerDomisili: Spinner = findViewById(R.id.spinnerDomisili)
+        val btnSubmit: Button = findViewById(R.id.btn_submit_register)
+        val checkBox: CheckBox = findViewById(R.id.checkBox)
+
+        btnSubmit.setOnClickListener {
+            val kotaDipilih = spinnerDomisili.selectedItem.toString()
+
+            if (checkBox.isChecked) {
+                Toast.makeText(
+                    this,
+                    "Registrasi berhasil! Anda memilih kota: $kotaDipilih",
+                    Toast.LENGTH_LONG
+                ).show()
+            } else {
+                Toast.makeText(
+                    this,
+                    "Harap setujui syarat dan ketentuan terlebih dahulu.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         }
     }
 }
